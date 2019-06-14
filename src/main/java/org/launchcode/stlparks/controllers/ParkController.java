c35a994b00668cf394fcd63ed9e119e63a214228
LAnderson-STL <kirsch_17@hotmail.com>
2019-06-13 23:01:18 -0500

    home index and show parks working

--- src/main/java/org/launchcode/stlparks/controllers/ParkController.java
+++ src/main/java/org/launchcode/stlparks/controllers/ParkController.java
@@ -1,6 +1,8 @@
 package org.launchcode.stlparks.controllers;
 
+import org.launchcode.stlparks.models.data.AmenityDao;
 import org.launchcode.stlparks.models.data.ParkDao;
+import org.launchcode.stlparks.models.Amenity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,7 +16,24 @@ public class ParkController {
     @Autowired
     private ParkDao parkDao;
 
+    @Autowired
+    private AmenityDao amenityDao;
 
+    @RequestMapping (value="", method = RequestMethod.GET)
+    public String index(Model model){
+
+        model.addAttribute("amenities", amenityDao.findAll());
+
+        return "park/index";
+    }
+
+    @RequestMapping (value="showParks", method=RequestMethod.GET)
+    public String showParks(Model model){
+
+        model.addAttribute("parks", parkDao.findAll());
+
+        return "park/showParks";
+    }
 
 
 
